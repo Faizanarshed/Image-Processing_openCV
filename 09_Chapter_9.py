b2aab99918ccb28c9c77ae272fe97d14c5af3384
Faizanarshed <96972195+Faizanarshed@users.noreply.github.com>
2022-02-05 15:45:47 +0500

    updated

--- 09_Chapter_9.py
+++ 09_Chapter_9.py
@@ -1,26 +1,25 @@
 # how to capture a webcam inside webcam
-
+# step 1 Import library
 import cv2 as cv
 import numpy as np
 
+#step 2 Read the frames from camera(webcame)
+
 cap = cv.VideoCapture(0)#webcam no 1
 
-#if (cap.isOpend()==False):
- #   print("there is error")
-# read untill the end
-
+# Step 3 Display Frame by Frame
 while(cap.isOpened()):
     #capture frame by frame
     ret,frame = cap.read()
     if ret == True:
         #to display frame
-        cv.imshow('frame',frame)
+        cv.imshow("Frame", frame)
         # to quite with q
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
 
-    
+# Step 4 : Relese or close all the Windows
 cv.waitKey(0)
 cv.destroyAllWindows()
