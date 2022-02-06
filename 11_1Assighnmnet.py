8b8387789933717950d353d9a2b9e32a9d69b986
Faizanarshed <96972195+Faizanarshed@users.noreply.github.com>
2022-02-06 17:28:12 +0500

    assighnment

---
+++ 11_1Assighnmnet.py
@@ -0,0 +1,22 @@
+# Assighnment
+#Import Library
+import numpy as np #numpy for array
+import cv2 as cv    #cv2 for openCV
+
+cap = cv.VideoCapture(0)    #activate the WebCam
+
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'XVID') #Set the Video Format
+out = cv.VideoWriter('resourses/output.avi', fourcc, 20.0, (640,  480))   #Video format and location of file where he will save
+while cap.isOpened():
+    ret, frame = cap.read() # Read the webcam convert it into frames
+    frame = cv.flip(frame, 1) # frame on flip 0 image will be 180 deg rotate
+    # write the flipped frame
+    out.write(frame)    #save the frame or video
+    cv.imshow('frame', frame) #Image show 
+    if cv.waitKey(1) == ord('q'): # wait key it will not let the disparse ina second
+        break
+# Release everything if job is finished
+cap.release()
+out.release()
+cv.destroyAllWindows()
