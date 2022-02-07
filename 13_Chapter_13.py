451933c4e2d9360832a421e51373891029198e91
Faizanarshed <96972195+Faizanarshed@users.noreply.github.com>
2022-02-07 21:02:55 +0500

    update

--- 13_Chapter_13.py
+++ 13_Chapter_13.py
@@ -1,30 +1,39 @@
 #basic functions or manuplation in open cv
 import cv2 as cv
+import numpy as np
 # import or add a picure
-img = cv.imread("resourses/img1.jpeg")
+img1 = cv.imread("resourses/img1.jpeg")
+img = cv.imread("resourses/image1.jpg")
 
 #resize img
-resize_img = cv.resize(img,(460,450))
+resize_img = cv.resize(img,(53,53))
 # Gray Img
 Gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
 # blures Image 
 
-blurr_img = cv.GaussianBlur(img, (7,7),0)
+blurr_img = cv.GaussianBlur(img, (3,3),0)
+# Black_White Image 
 
+(thresh,binaryimg) = cv.threshold(Gray_img,127,255,cv.THRESH_BINARY)
 # edge detection
 edge_img = cv.Canny(img, 48,48)
 
 #thikness of lines
-dilated_img = cv.dilate(edge_img,(7,7),iterations=1)
+mat_kernel = np.ones((7,7), np.uint8)
+dilated_img = cv.dilate(edge_img,(mat_kernel),(7,7),iterations=1)
+# make a Thinner OutLine
+ero_img = cv.erode(dilated_img,mat_kernel,iterations=1)
 
-
-cv.imshow('original_image', img)
+#cv.imshow('original_image', img)
 #cv.imshow('ReShape_image', resize_img)
 #cv.imshow('Gray_image', Gray_img)
 #cv.imshow('Blur_image', blurr_img)
-cv.imshow('Edge', edge_img)
-cv.imshow('Dilated_image', dilated_img)
+#cv.imshow('Edge', edge_img)
+#cv.imshow('Dilated_image', dilated_img)
+#cv.imshow('mat_kernal',mat_kernel)
+#cv.imshow('Ero_image',ero_img)
+cv.imshow('Black_White Image',binaryimg)
 #cv.imshow("frame",frame)
 cv.waitKey(0)
 cv.destroyAllWindows()
