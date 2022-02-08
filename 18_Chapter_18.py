30911b5eb7c284ce14c41914794fa2262aa281c5
Faizanarshed <96972195+Faizanarshed@users.noreply.github.com>
2022-02-08 23:37:40 +0500

    update

--- 18_Chapter_18.py
+++ 18_Chapter_18.py
@@ -6,10 +6,15 @@ import numpy as np
 
 img = cv.imread("resourses/img1.jpeg")
 print(img.shape)
+
+height = 900
+weidth = 800
+
+height,weidth = 900,800
 # Defineing a point
 
 
-#point1 =  np.float32([[233,196],[82,471],[522,169],[715,482]])
+point1 =  np.float32([[233,196],[82,471],[522,169],[715,482]])
 
 
 
