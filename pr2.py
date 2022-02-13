234d386cacdd2dd446a27fe8127c0566cb315f56
Faizanarshed <96972195+Faizanarshed@users.noreply.github.com>
2022-02-13 23:40:45 +0500

    update

--- pr2.py
+++ pr2.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 import cv2 as cv
 
 
@@ -8,5 +9,9 @@ path = "resourses/image1.jpeg"
 cv.namedWindow("Bars")
 cv.resizeWindow("Bars", 1020, 400)
 cv.createTrackbar("Hue Min", "Bars" , 0 ,255 , slider)
+cv.createTrackbar("Hue Max", "Bars" , 0 ,255 , slider)
+cv.createTrackbar("Stu Min", "Bars" , 0 ,255 , slider)
+cv.createTrackbar("Stu Max", "Bars" , 0 ,255 , slider)
+
 cv.waitKey(0)
 cv.destroyAllWindows()
