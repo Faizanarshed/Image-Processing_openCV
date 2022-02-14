2f800a09c7e8d2f4c2d6d05076cd03cf2276e084
Faizanarshed <96972195+Faizanarshed@users.noreply.github.com>
2022-02-14 21:55:07 +0500

    update

--- 22_Chapter_22.py
+++ 22_Chapter_22.py
@@ -2,9 +2,16 @@ import cv2 as cv
 import numpy as np
 
 
-fsce_cascade = cv.CascadeClassifier()
+face_cascade = cv.CascadeClassifier("resourses/haarcascade_frontalface_default.xml")
 
-img = cv.imread("resourses/img2.jpeg")
+img = cv.imread("resourses/image1.jpg")
+
+gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray_img,1.1,4)
+#draw a rectangle
+for (x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
 
 print(img.shape)
 
@@ -13,5 +20,6 @@ print(img.shape)
 
 
 cv.imshow("image",img)
+cv.imwrite("resourses/faces.jpg",img)
 cv.waitKey(0)
 cv.destroyAllWindows()
